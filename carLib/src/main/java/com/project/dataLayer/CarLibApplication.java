7864895e69a61e32430ffa90d83549c8c72e5d1f
Your Name <you@example.com>
2024-01-19 18:11:34 +0100

    setUP

---
+++ carLib/src/main/java/com/project/dataLayer/CarLibApplication.java
@@ -0,0 +1,13 @@
+package com.project.dataLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarLibApplication.class, args);
+	}
+
+}
