a9ca29b8b61477466c04112844a8c5c6fdc42f9e
Your Name <you@example.com>
2024-01-19 22:04:17 +0100

    add the spring project

---
+++ carLib/src/test/java/com/project/dataLayer/CarLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dataLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
